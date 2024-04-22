686d1884716eef3134cb30408caba48933e5c69d
dasirrine <54993753+dasirrine@users.noreply.github.com>
2024-04-22 15:33:46 -0400

    Update configDumpFiles.ps1
    source URL added, additional error checking

--- configDumpFiles.ps1
+++ configDumpFiles.ps1
@@ -1,15 +1,16 @@
 
 # configDumpFiles.ps1 by David Sirrine @ CNS Computer Services, LLC
+# source: https://github.com/dasirrine/Syncro/blob/main/configDumpFiles.ps1
 # latest revision 2024-04-22
 
-# Sets the Crash (BSoD) settings to create full, kernel-only, small, or
+# Sets Windows Crash (BSoD) settings to create full, kernel-only, small, or
 # no memory dumps and keep all or only the last memory dump.
 #
 # Defaults to "Complete" for debugging purposes,
 # and "overwrite" to save disk space.
 
 
-#default dump level "Complete"
+#set default dump level to "Complete"
 if {-not $dumpLevel) { $dumpLevel = "Complete" }
 
 #hash to decode human-readable dump levels
@@ -22,11 +23,11 @@ $lvlHash = @{
 $lvl = $lvlHash[$dumpLevel]
 
 #set the crash memory dump level
-if (-not $lvl) {
-    Write-Host "Invalid dump level specified."
+if (($lvl) -and ($lvl -in 0..3)) {
+	"Crash dump files will be set to level: `"$dumpLevel`""
+	Get-WmiObject -Class Win32_OSRecoveryConfiguration -EnableAllPrivileges | Set-WmiInstance -Arguments @{ DebugInfoType=$lvl }
 } else {
-	Write-Host "$dumpLevel crash dump files will be saved."
-    Get-WmiObject -Class Win32_OSRecoveryConfiguration -EnableAllPrivileges | Set-WmiInstance -Arguments @{ DebugInfoType=$lvl }
+    Write-Host "Invalid dump level specified."
 }
 
 #overwrite memory dump files unless specified
@@ -34,7 +35,7 @@ if ($overwriteDebugFiles="no") {
 	Write-Host "ALL dump files will be retained."
 	$CrashBehaviour | Set-WmiInstance -Arguments @{ OverwriteExistingDebugFile=$False }
 	###warn if "Complete" dump files will be retained
-	if ($lvl=1) { Write-Host "WARNING: Be aware that `"Complete`" dump files can quickly fill the drive!" }
+	if ($lvl=1) { Write-Host "WARNING: Saving all `"Complete`" dump files can quickly fill the drive!" }
 } else {
 	Write-Host "only the last dump file will be saved."
 	$CrashBehaviour | Set-WmiInstance -Arguments @{ OverwriteExistingDebugFile=$True }
